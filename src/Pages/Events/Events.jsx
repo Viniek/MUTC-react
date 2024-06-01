fec1207cbce13e8765a4afad014a93e0b16a82aa
unknown <njerivictory52@gmail.com>
2024-06-01 13:13:47 +0300

    commit

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -4,33 +4,33 @@ function Events() {
   return (
     <>
     <div className="events">
-      <div className="events1">
-        <div className="events2">
+      <div className="ourevents1">
+        <div className="ourevents2">
           <h1>cybersecurity</h1>
           <p>Every Monday from 4pm to 7pm at computer lab F04, led by Webster Ifedha.</p>
         </div>
         
-        <div className="events2">
+        <div className="ourevents2">
           <h1>UI/UX</h1>
           <p>Every Tuesday from 5pm to 8pm at computer lab F04, led by Manase Gunga..</p>
         </div>
 
-        <div className="events2">
+        <div className="ourevents2">
           <h1>Webdevelopment</h1>
           <p>Every Wednesday from 6pm to 9pm at computer lab F04, led by Carolyne Githenduka..</p>
         </div>
 
-        <div className="events2">
+        <div className="ourevents2">
           <h1>Mobile apps development </h1>
           <p>Every Thursday from 4pm to 7pm led by Stanley Amunze.</p>
         </div>
 
-        <div className="events2">
+        <div className="ourevents2">
           <h1>Cloud engineering</h1>
           <p>Every Friday from 3pm to 6pm at computer lab F04, led by Paul Karanja.</p>
         </div>
 
-        <div className="events2">
+        <div className="ourevents2">
           <h1>Power Platform</h1>
           <p>Every Saturday from 1pm to 4pm at computer lab F04, led by Evyonn Mbithe.</p>
         </div>
