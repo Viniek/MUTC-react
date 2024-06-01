482de067cf41f792fb2f5d993390449f30383d31
unknown <njerivictory52@gmail.com>
2024-06-01 11:46:58 +0300

    commit

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -1,11 +1,87 @@
 import "./Tracks.css";
 function Tracks() {
-  return 
-<>
-<section className="1">
-  <h1><i>Explore a world of innovation with my tracks </i></h1>
-  <h2>where creativity meets technology</h2>
-</section>
-</>
-}  
+  return <div>
+
+    <section className="header">
+      <div className="top">
+      <h1>  <i>Explore a world of innovation with our tracks</i></h1>
+      <h2>where creativity meets technology</h2>
+      </div>
+  
+      <div className="tracks">
+
+        <div className="tracks1">
+        <div className="cybersecurity">
+          <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>
+        <div className="cybersecurity">
+        <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>
+        <div className="cybersecurity">
+        <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>    
+        </div>
+        <div className="tracks2">   
+        <div className="cybersecurity">
+        <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>
+        
+        
+        <div className="cybersecurity">
+        <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>
+        <div className="cybersecurity">
+        <h1>cybersecurity</h1><br></br>
+        <p>Students who choose the Cyber Security track will delve into the critical 
+        aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
+        will learn about various security protocols, ethical hacking, encryption techniques, and threat
+         analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
+          in identifying and mitigating security vulnerabilities. By the end of the track, students will be 
+          equipped with the skills to safeguard digital environments, conduct security audits, and implement
+           robust security measures to protect against cyber attacks</p>
+        </div>
+        </div>
+      </div>
+
+      
+
+
+    </section>
+  </div>;
+}
 export default Tracks;
