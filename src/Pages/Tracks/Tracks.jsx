c76eca15e8221d0bcdf9038c8c4d6bb2e6e4abf6
unknown <njerivictory52@gmail.com>
2024-06-01 19:38:23 +0300

    commit

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -33,10 +33,7 @@ function Tracks() {
         <p>Students who pursue the Web Development track will gain comprehensive knowledge in building dynamic 
           and responsive websites. They will learn the fundamentals of front-end development using HTML, CSS,
            and JavaScript, as well as back-end development with technologies such as Node.js, Express, and 
-           databases. Through hands-on projects and collaborative coding sessions, students will develop the 
-           skills to create both the client-side and server-side of web applications. By the end of the track,
-            students will be capable of designing, developing, and deploying full-stack web applications, 
-            equipped with the expertise to build robust and scalable websites.</p>
+           databases.</p>
         </div>    
         </div>
         <div className="tracks2">   
@@ -46,33 +43,36 @@ function Tracks() {
            for mobile devices. They will explore the basics of mobile app development using platforms such as 
            Android and iOS, gaining proficiency in languages like Java, Kotlin, Swift, and Dart. Through
             hands-on projects and interactive sessions, students will develop the skills to create 
-            user-friendly and efficient mobile applications. By the end of the track, students will be adept
-             at building, testing, and deploying mobile apps, ready to deliver engaging and high-performance
-              solutions for smartphones and tablets.</p>
+            user-friendly and efficient mobile applications.</p>
         </div>
         
         
         <div className="cybersecurity">
         <h1>Cloud engineering</h1><br></br>
         <p>Students who choose the Cloud Engineering track will dive into the world of cloud computing and its 
-          applications. They will learn about various cloud service providers such as AWS, Google Cloud, and
-           Microsoft Azure, and understand how to design, deploy, and manage scalable and secure cloud
-            infrastructures. Through practical projects and hands-on labs, students will gain experience in cloud 
-            architecture, virtualization, containerization, and DevOps practices. By the end of the track, students
+          applications . By the end of the track, students
              will be capable of leveraging cloud technologies to build and maintain robust cloud-based solutions,
               ensuring high availability and performance for applications and services.</p>
         </div>
         <div className="cybersecurity">
         <h1>Power Platform</h1><br></br>
         <p>Students who select the Power Platform track will explore the capabilities of Microsoft's Power
-           Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They will 
-           learn how to create custom business applications, automate workflows, analyze data, and build 
-           chatbots with minimal coding. Through hands-on projects and interactive workshops, students will
-            gain practical experience in leveraging these tools to enhance business processes and drive 
-            data-driven decision-making. By the end of the track, students will be proficient in developing 
-            integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.</p>
+           Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. By the end of the track, students will be proficient in developing 
+            integrated solutions using the Power Platform, enabling them to transform organizational operations 
+            and improve efficiency.</p>
         </div>
         </div>
+      </div>
+      <div className="form">
+        <form>
+          {/* fname */}
+          <label htmlFor="fname">Firstname</label><br></br>
+          <input type="text"id="fname"placeholder="Enter your full name..." required></input>
+          {/* lname */}
+            {/* fname */}
+            <label htmlFor="email">emaik</label><br></br>
+          <input type="email"id="email"placeholder="Enter your email address..." required></input>
+        </form>
       </div>
 
       
