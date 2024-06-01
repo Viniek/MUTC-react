d445f4619bb5bdb590ad01bda302e3300ed2e544
unknown <njerivictory52@gmail.com>
2024-06-01 11:24:53 +0300

    commit

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -62,7 +62,7 @@ function Tracks() {
            robust security measures to protect against cyber attacks</p>
         </div>
         <div className="cybersecurity">
-        <p>Cyber Security <br/>Students who choose the Cyber Security track will delve into the critical 
+        <p>Students who choose the Cyber Security track will delve into the critical 
         aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they 
         will learn about various security protocols, ethical hacking, encryption techniques, and threat
          analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge
