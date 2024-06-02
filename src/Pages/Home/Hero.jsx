d628668d4aa0321d4de3c0609acc6d0ad2fc0b3f
unknown <njerivictory52@gmail.com>
2024-06-02 09:41:44 +0300

    --

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
     <section className="hero">
       <h1 className="mission">our mission</h1>
       <p className="themission">
-        To advance knowledge and technological transfer through teaching,
+        <i>To advance knowledge and technological transfer through teaching,
         training, learning, research, innovation, consultancy and community
-        engagement for sustainable development.
+        engagement for sustainable development.</i>
       </p>
       <Link to="/aboutus">About us</Link>
     </section>
